50c6ad4c3eb77e11e9b69ae99f6dc1e5f0ec32b1
buk1t <grandee.sculler-2c@icloud.com>
2026-01-27 22:03:33 -0500

    final

--- bootstrap/setup.py
+++ bootstrap/setup.py
@@ -1,21 +1,35 @@
-# bootstrap/setup.py
 from setuptools import setup
 
 APP = ["main.py"]
 
+DATA_FILES = [
+    ("web", ["../web/index.html", "../web/style.css", "../web/app.js"]),
+    ("data", []),
+    ("", ["../version.json"]),
+]
+
 OPTIONS = {
     "argv_emulation": False,
-    "packages": ["webview"],
-    "includes": [],
+    "packages": ["webview", "bottle", "proxy_tools"],
+    "includes": [
+        "urllib",
+        "urllib.request",
+        "urllib.parse",
+        "urllib.error",
+        "ssl",
+        "http.client",
+    ],
+    "excludes": ["pip", "setuptools", "pkg_resources", "wheel", "test", "unittest"],
     "plist": {
-        "CFBundleName": "mood",
-        "CFBundleDisplayName": "mood",
+        "CFBundleName": "Mood",
+        "CFBundleDisplayName": "Mood",
         "CFBundleIdentifier": "com.buk1t.mood",
     },
 }
 
 setup(
     app=APP,
+    data_files=DATA_FILES,
     options={"py2app": OPTIONS},
     setup_requires=["py2app"],
 )
